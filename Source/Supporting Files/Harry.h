c2ba121d3b7e6fbe6dfb3a69fa75a2993de254df
Gavan Chan <gavan.chan@outware.com.au>
2016-07-31 14:51:04 +1000

    Rejig pre-generated files
    * Make Harry scheme visible to Carthage—by sharing it
    * Rename the project navigator groups in Xcode—and reflect this in the file system
    * Remove unnecessary XCTest file
    * Slim down pre-generated header

---
+++ Source/Supporting Files/Harry.h
@@ -0,0 +1,7 @@
+// Copyright © 2016 Gavan Chan. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double HarryVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char HarryVersionString[];
