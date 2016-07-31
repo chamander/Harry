0712bd103a5225543761fd8db0ee279d670eaf07
Gavan Chan <gavan.chan@outware.com.au>
2016-07-31 15:25:55 +1000

    Set up framework dependencies
    * Import Quick and Nimble into the project
    * Reduce project language dependency to Swift 2.3 (derp, I created this project in the beta, so it was using 3.0)
    * Stop the Info.plist file(s) from being bundled into the app
    * Introduce a placeholder test so that Swiftlint does not fail the build

---
+++ Tests/TechSpec.swift
@@ -0,0 +1,20 @@
+// Copyright © 2016 Gavan Chan. All rights reserved.
+
+final class TechSpec: QuickSpec {
+
+  override func spec() {
+
+    describe("this test") {
+
+      it("runs") {
+        expect(true).to(be(true))
+      }
+
+    }
+
+  }
+
+}
+
+import Quick
+import Nimble
